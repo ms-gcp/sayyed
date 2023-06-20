2e5393f5e21e7ce44e84c8977ccd3e8698e14b18
ms-gcp <ms.gcp123@gmail.com>
2023-06-20 09:03:21 +0530

    ...

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-## Load the model....
+## Load the model........
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
